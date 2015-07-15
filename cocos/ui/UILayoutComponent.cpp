66d69d7a44f726718253abc5d345f99e1b9f97ba
Liam <magic_new_world@163.com>
2015-07-15 09:35:19 +0800

    update for layout

--- cocos/ui/UILayoutComponent.cpp
+++ cocos/ui/UILayoutComponent.cpp
@@ -287,7 +287,7 @@ namespace ui {
             {
                 _positionPercentX = 0;
                 ownerPoint.x = 0;
-                if (_usingPositionPercentX)
+                if (_usingPositionPercentX || _horizontalEdge == HorizontalEdge::Center)
                     _owner->setPosition(ownerPoint);
             }
 
@@ -318,7 +318,7 @@ namespace ui {
             {
                 _positionPercentY = 0;
                 ownerPoint.y = 0;
-                if (_usingPositionPercentY)
+                if (_usingPositionPercentY || _verticalEdge == VerticalEdge::Center)
                     _owner->setPosition(ownerPoint);
             }
 
